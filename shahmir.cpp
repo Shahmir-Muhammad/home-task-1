12c8fbbd46b8ccbb57bebf0fde4750522e07af4f
Shahmir-Muhammad <147588981+Shahmir-Muhammad@users.noreply.github.com>
2023-10-10 23:56:18 -0700

    Add files via upload

---
+++ shahmir.cpp
@@ -0,0 +1,75 @@
+ #include <iostream>
+ using namespace std;
+ int main ()
+ { 
+ /*float fa=0,fb=0,fc=0;
+ cout<<"enter length in centimeter:";
+ cin>>fa;
+  fb=fa/100;
+ ;cout<<"length in meters:"<<fb<<endl;
+ fc=fa/100000;
+ cout<<"length in km is:"<<fc<<endl;*/
+ 
+ 
+/* float x=0,y=0,z=0;
+ cout<<"enter temp in fahrenheit:";
+ cin>>x;
+ y=x-32;
+z=y*0.556;
+ cout<<"temp in centigrade is:"<<z<<endl;
+ return 0;*/
+ 
+
+f=ax-cx;
+g=by-dy;
+h=f*f;
+i=g*g;
+j=h+i;
+cout<<"distance btw two point
+ 
+  
+float ax=0,by=0,cx=0,dy=0,f=0,g=0,h=0,i=0,j=0;
+cout<<"enter x1"<<endl;
+cin>>ax;
+cout<<"enter y1"<<endl;
+cin>>by;
+cout<<"enter x2"<<endl;
+cin>>cx;
+cout<<"enter y2"<<endl;
+cin>>dy;s is:"<<j<<endl;
+ 
+ return 0;
+ 	
+ }
+ 
+ 
+  
+/*float ax=0,by=0,cx=0,dy=0,f=0,g=0,h=0,i=0,j=0;
+cout<<"enter x1"<<endl;
+cin>>ax;
+cout<<"enter y1"<<endl;
+cin>>by;
+cout<<"enter x2"<<endl;
+cin>>cx;
+cout<<"enter y2"<<endl;
+cin>>dy;
+f=ax-cx;
+g=by-dy;
+h=f*f;
+i=g*g;
+j=h+i;
+cout<<"distance btw two points is:"<<j<<endl;
+return 0;*/
+
+
+/*float a=0,b=0,c=0,d=0;
+cout<<"enter a";
+cin>>a;
+cout<<"enter b";
+cin>>b;
+c=a+b;
+d=c*c;
+cout<<"ans of polynomial with a and b values;"<<d<<endl;*/
+
+
+
